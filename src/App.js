2672c45928ecf3296541b8736c3538bff29d9c21
Barbara Castilho <barbara.mcastilho@gmail.com>
2023-05-07 19:00:13 +0100

    redesign frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Board from './components/Board';
+import Footer from './components/Footer';
 
 class App extends Component {
     render() {
         return (
-            <>
+            <section className="centered-content">
                 <Header />
                 <Board />
-            </>
+                <Footer />
+            </section>
         );
     }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <p>
+          proudly developed with react by Bárbara Castilho  
+        </p>
+    )
+}
+export default Footer;
